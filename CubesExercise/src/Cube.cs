f4ae1a6b9c155c7104c71c46024690b59bb9eac3
mjtordesillas <mjtordesillas@codesai.com>
2017-06-25 16:15:06 +0200

    green: volume intersection should be correct as long as the y,z coordinates are fixed and the second cube is more to the right

--- CubesExercise/src/Cube.cs
+++ CubesExercise/src/Cube.cs
@@ -11,9 +11,33 @@
             this.edgeLength = edgeLength;
         }
 
+        public double MinX
+        {
+            get { return point.X - ProjectedDistanceFromCenter; }
+        }
+
+        public double MaxX
+        {
+            get { return point.X + ProjectedDistanceFromCenter; }
+        }
+
+        private double ProjectedDistanceFromCenter
+        {
+            get { return edgeLength / 2.0; }
+        }
+
         public double intersectionVolumeWith(Cube cube)
         {
-            return 0;
+            if (MaxX >= cube.MinX && MinX <= cube.MaxX )
+            {
+                return (MaxX - cube.MinX)
+                    * edgeLength
+                    * edgeLength;
+            }
+            else
+            {
+                return 0;
+            }
         }
     }
 }
--- CubesExercise/test/CubeIntersectionShould.cs
+++ CubesExercise/test/CubeIntersectionShould.cs
@@ -14,5 +14,14 @@ namespace CubesExercise
 
             Assert.AreEqual(0, cubeA.intersectionVolumeWith(cubeB));
         }
+
+        [TestMethod]
+        public void be_correct_for_intersecting_cubes_with_height_and_depth_fixed_as_long_as_the_second_is_more_to_the_right()
+        {
+            Cube cubeA = new Cube(new Point(2, 2, 2), 2);
+            Cube cubeB = new Cube(new Point(3, 2, 2), 2);
+
+            Assert.AreEqual(4, cubeA.intersectionVolumeWith(cubeB));
+        }
     }
 }
