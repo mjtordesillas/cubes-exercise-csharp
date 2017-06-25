201f66d0cd8af5a2f601c4d4933e05c182f1e736
mjtordesillas <mjtordesillas@codesai.com>
2017-06-25 23:35:03 +0200

    refactor: using builder pattern to reduce duplication when creating cubes

--- CubesExercise/test/CubeIntersectionShould.cs
+++ CubesExercise/test/CubeIntersectionShould.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using static CubesExercise.CubeBuilder;
 
 namespace CubesExercise
 {
@@ -10,8 +11,8 @@ namespace CubesExercise
         [TestMethod]
         public void be_zero_when_cubes_do_not_intersect()
         {
-            Cube cubeA = new Cube(new Point(2, 2, 2), 2);
-            Cube cubeB = new Cube(new Point(10, 10, 10), 2);
+            var cubeA = Cube().CenteredAt(2, 2, 2).WithEdgeLength(2).Build();
+            var cubeB = Cube().CenteredAt(10, 10, 10).WithEdgeLength(2).Build();
 
             Assert.AreEqual(0, cubeA.IntersectionVolumeWith(cubeB));
             Assert.AreEqual(0, cubeB.IntersectionVolumeWith(cubeA));
@@ -20,8 +21,8 @@ namespace CubesExercise
         [TestMethod]
         public void be_correct_for_intersecting_cubes_with_height_and_depth_fixed()
         {
-            Cube cubeA = new Cube(new Point(2, 2, 2), 2);
-            Cube cubeB = new Cube(new Point(3, 2, 2), 2);
+            var cubeA = Cube().CenteredAt(2, 2, 2).WithEdgeLength(2).Build();
+            var cubeB = Cube().CenteredAt(3, 2, 2).WithEdgeLength(2).Build();
 
             Assert.AreEqual(4, cubeA.IntersectionVolumeWith(cubeB));
             Assert.AreEqual(4, cubeB.IntersectionVolumeWith(cubeA));
@@ -30,8 +31,8 @@ namespace CubesExercise
         [TestMethod]
         public void be_correct_for_intersecting_cubes_with_width_and_depth_fixed()
         {
-            Cube cubeA = new Cube(new Point(2, 2, 2), 2);
-            Cube cubeB = new Cube(new Point(2, 3, 2), 2);
+            var cubeA = Cube().CenteredAt(2, 2, 2).WithEdgeLength(2).Build();
+            var cubeB = Cube().CenteredAt(2, 3, 2).WithEdgeLength(2).Build();
 
             Assert.AreEqual(4, cubeA.IntersectionVolumeWith(cubeB));
             Assert.AreEqual(4, cubeB.IntersectionVolumeWith(cubeA));
@@ -40,8 +41,8 @@ namespace CubesExercise
         [TestMethod]
         public void be_correct_for_intersecting_cubes_with_width_and_height_fixed()
         {
-            Cube cubeA = new Cube(new Point(2, 2, 2), 2);
-            Cube cubeB = new Cube(new Point(2, 2, 3), 2);
+            var cubeA = Cube().CenteredAt(2, 2, 2).WithEdgeLength(2).Build();
+            var cubeB = Cube().CenteredAt(2, 2, 3).WithEdgeLength(2).Build();
 
             Assert.AreEqual(4, cubeA.IntersectionVolumeWith(cubeB));
             Assert.AreEqual(4, cubeB.IntersectionVolumeWith(cubeA));
@@ -50,8 +51,8 @@ namespace CubesExercise
         [TestMethod]
         public void be_equal_to_the_volume_of_the_smaller_cube_when_it_is_contained_by_the_bigger_cube()
         {
-            Cube cubeA = new Cube(new Point(2, 2, 2), 2);
-            Cube cubeB = new Cube(new Point(2, 2, 2), 1);
+            var cubeA = Cube().CenteredAt(2, 2, 2).WithEdgeLength(2).Build();
+            var cubeB = Cube().CenteredAt(2, 2, 2).WithEdgeLength(1).Build();
 
             Assert.AreEqual(1, cubeA.IntersectionVolumeWith(cubeB));
             Assert.AreEqual(1, cubeB.IntersectionVolumeWith(cubeA));
@@ -60,8 +61,8 @@ namespace CubesExercise
         [TestMethod]
         public void be_equal_to_the_volume_of_any_cube_when_they_are_the_same()
         {
-            Cube cubeA = new Cube(new Point(2, 2, 2), 2);
-            Cube cubeB = new Cube(new Point(2, 2, 2), 2);
+            var cubeA = Cube().CenteredAt(2, 2, 2).WithEdgeLength(2).Build();
+            var cubeB = Cube().CenteredAt(2, 2, 2).WithEdgeLength(2).Build();
 
             Assert.AreEqual(8, cubeA.IntersectionVolumeWith(cubeB));
             Assert.AreEqual(8, cubeB.IntersectionVolumeWith(cubeA));
@@ -70,11 +71,34 @@ namespace CubesExercise
         [TestMethod]
         public void be_zero_when_cubes_are_touching_but_not_intersecting()
         {
-            Cube cubeA = new Cube(new Point(2, 2, 2), 2);
-            Cube cubeB = new Cube(new Point(4, 2, 2), 2);
+            var cubeA = Cube().CenteredAt(2, 2, 2).WithEdgeLength(2).Build();
+            var cubeB = Cube().CenteredAt(4, 2, 2).WithEdgeLength(2).Build();
 
             Assert.AreEqual(0, cubeA.IntersectionVolumeWith(cubeB));
             Assert.AreEqual(0, cubeB.IntersectionVolumeWith(cubeA));
+        }
+    }
+    class CubeBuilder
+    {
+        private Point center;
+        private double edgeLength;
+        public static CubeBuilder Cube()
+        {
+            return new CubeBuilder();
+        }
+        public CubeBuilder CenteredAt(double x, double y, double z)
+        {
+            center = new Point(x, y, z);
+            return this;
+        }
+        public CubeBuilder WithEdgeLength(double length)
+        {
+            edgeLength = length;
+            return this;
+        }
+        public Cube Build()
+        {
+            return new Cube(center, edgeLength);
         }
     }
 }
