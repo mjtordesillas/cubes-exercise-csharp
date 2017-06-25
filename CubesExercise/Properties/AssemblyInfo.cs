cae4194a981f113514e16c57508d4e84e2e7367e
mjtordesillas <mjtordesillas@codesai.com>
2017-06-25 15:37:58 +0200

    Add project files.

---
+++ CubesExercise/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CubesExercise")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CubesExercise")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4178b6d6-8e84-4a13-b14e-44e422ef8e9c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ CubesExercise/test/CubeIntersectionShould.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CubesExercise
+{
+    [TestClass]
+    public class CubeIntersectionShould
+    {
+        [TestMethod]
+        public void fail()
+        {
+            Assert.Fail();
+        }
+    }
+}
