5376369b19de6f33af7a8b68077f8e847af9e529
mjtordesillas <mjtordesillas@codesai.com>
2017-06-25 15:57:27 +0200

    refactor: changing the name of test

--- CubesExercise/test/CubeIntersectionShould.cs
+++ CubesExercise/test/CubeIntersectionShould.cs
@@ -7,7 +7,7 @@ namespace CubesExercise
     public class CubeIntersectionShould
     {
         [TestMethod]
-        public void return_zero_when_cubes_do_not_intersect()
+        public void be_zero_when_cubes_do_not_intersect()
         {
             Cube cubeA = new Cube(new Point(2, 2, 2), 1);
             Cube cubeB = new Cube(new Point(10, 10, 10), 1);
