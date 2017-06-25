ce35748a222f6fb41b28f60946ae81146055dfa7
mjtordesillas <mjtordesillas@codesai.com>
2017-06-25 23:30:38 +0200

    refactor: simplified code by introducing Edge value object

--- CubesExercise/src/Cube.cs
+++ CubesExercise/src/Cube.cs
@@ -4,76 +4,27 @@ namespace CubesExercise
 {
     public class Cube
     {
-        private Point point;
-        private double edgeLength;
+        private Point Center { get; }
+        private double EdgeLength { get; }
 
-        public Cube(Point point, double edgeLength)
+        private Edge width;
+        private Edge height;
+        private Edge depth;
+
+        public Cube(Point center, double edgeLength)
         {
-            this.point = point;
-            this.edgeLength = edgeLength;
+            Center = center;
+            EdgeLength = edgeLength;
+            width = new Edge(center.X, EdgeLength);
+            height = new Edge(center.Y, EdgeLength);
+            depth = new Edge(center.Z, EdgeLength);
         }
-
-        private double MinX
-        {
-            get { return point.X - ProjectedDistanceFromCenter; }
-        }
-
-        private double MaxX
-        {
-            get { return point.X + ProjectedDistanceFromCenter; }
-        }
-
-        private double MinY
-        {
-            get { return point.Y - ProjectedDistanceFromCenter; }
-        }
-
-        private double MaxY
-        {
-            get { return point.Y + ProjectedDistanceFromCenter; }
-        }
-
-        private double MinZ
-        {
-            get { return point.Z - ProjectedDistanceFromCenter; }
-        }
-
-        private double MaxZ
-        {
-            get { return point.Z + ProjectedDistanceFromCenter; }
-        }
-
-        private double ProjectedDistanceFromCenter
-        {
-            get { return edgeLength / 2.0; }
-        }
-
+ 
         public double IntersectionVolumeWith(Cube cube)
         {
-            return WidthOverlap(cube)
-                * HeightOverlap(cube)
-                * DepthOverlap(cube);
+            return width.Overlap(cube.width)
+                * height.Overlap(cube.height)
+                * depth.Overlap(cube.depth);
         }
-
-        private double WidthOverlap(Cube cube)
-        {
-            return ProjectedSegmentsOverlap(MinX, MaxX, cube.MinX, cube.MaxX);
-        }
-
-        private double HeightOverlap(Cube cube)
-        {
-            return ProjectedSegmentsOverlap(MinY, MaxY, cube.MinY, cube.MaxY);
-        }
-
-        private double DepthOverlap(Cube cube)
-        {
-            return ProjectedSegmentsOverlap(MinZ, MaxZ, cube.MinZ, cube.MaxZ);
-        }
-
-        private double ProjectedSegmentsOverlap(double minCoordinate1, double maxCoordinate1, double minCoordinate2, double maxCoordinate2)
-        {
-            return Math.Max(0, Math.Min(maxCoordinate1, maxCoordinate2) - Math.Max(minCoordinate1, minCoordinate2));
-        }
-
     }
 }
---
+++ CubesExercise/src/Edge.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CubesExercise
+{
+    public class Edge
+    {
+        private double start;
+        private double end;
+
+        public Edge(double center, double length)
+        { 
+            start = center - length / 2.0;
+            end = center + length / 2.0;
+        }
+
+        public double Overlap(Edge edge)
+        {
+            return Math.Max(0, Math.Min(end, edge.end) -
+                Math.Max(start, edge.start));
+        }
+    }
+}
